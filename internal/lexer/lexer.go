153bad10638df45c048e2c8a59927606072bd2bb
Jim Schubert <james.schubert@gmail.com>
2026-03-17 18:48:07 -0400

    WIP: lexer keywords/mapping

--- internal/lexer/lexer.go
+++ internal/lexer/lexer.go
@@ -17,20 +17,221 @@ import (
 type itemType int
 
 const (
-	itemError itemType = iota // error occurred; value is text of error
-
-	itemNamespace
-	itemText
+	// special tokens
+	itemError itemType = iota
+	itemUnrecognized
 	itemEOF
+	itemNewline
+
+	// declarations
+	itemKeywordNamespace
+	itemKeywordImport
+	itemKeywordType
+	itemKeywordEnum
+	itemKeywordModel
+	itemKeywordInput
+	itemKeywordAPI
+
+	// API-related
+	itemKeywordRPC
+	itemKeywordStream
+	itemKeywordPublish
+	itemKeywordSubscribe
+
+	// HTTP methods
+	itemKeywordGET
+	itemKeywordPOST
+	itemKeywordPUT
+	itemKeywordPATCH
+	itemKeywordDELETE
+	itemKeywordHEAD
+	itemKeywordOPTIONS
+
+	// decorators
+	itemKeywordVersion
+	itemKeywordStyle
+	itemKeywordBasePath
+	itemKeywordAuth
+	itemKeywordBody
+	itemKeywordQuery
+	itemKeywordErrors
+	itemKeywordSummary
+	itemKeywordTag
+	itemKeywordDeprecated
+	itemKeywordRaw
+	itemKeywordDefault
+	itemKeywordPrimary
+	itemKeywordUnique
+	itemKeywordUpdatedAt
+	itemKeywordRelation
+
+	// API styles
+	itemKeywordREST
+	itemKeywordEvents
+
+	// Auth
+	itemKeywordBearer
+	itemKeywordAPIKey
+	itemKeywordBasic
+	itemKeywordNone
+
+	// type modifiers and literals
+	itemKeywordTrue
+	itemKeywordFalse
+	itemKeywordNull
+	itemKeywordNow
+	itemKeywordUUID
+
+	// scalars
+	itemKeywordString
+	itemKeywordInt
+	itemKeywordFloat
+	itemKeywordBoolean
+	itemKeywordTimestamp
+	itemKeywordDate
+	itemKeywordAny
+
+	// ident/literals
+	itemIdent
+	itemString
+	itemInt
+	itemFloat
+
+	// other common
+	itemLeftBrace
+	itemRightBrace
+	itemLeftParen
+	itemRightParen
+	itemLeftBracket
+	itemRightBracket
+	itemLeftAngle
+	itemRightAngle
+	itemColon
+	itemComma
+	itemDot
+	itemDash
+	itemArrow
+	itemAt
+	itemEquals
+	itemQuestion
+	itemSlash
+
+	// comments
+	itemComment
 )
 
 const (
-	keywordNamespace = "namespace"
-	keywordImport    = "import"
+	keywordNamespace  = "namespace"
+	keywordImport     = "import"
+	keywordType       = "type"
+	keywordEnum       = "enum"
+	keywordModel      = "model"
+	keywordInput      = "input"
+	keywordAPI        = "api"
+	keywordRPC        = "rpc"
+	keywordStream     = "stream"
+	keywordPublish    = "publish"
+	keywordSubscribe  = "subscribe"
+	keywordGET        = "GET"
+	keywordPOST       = "POST"
+	keywordPUT        = "PUT"
+	keywordPATCH      = "PATCH"
+	keywordDELETE     = "DELETE"
+	keywordHEAD       = "HEAD"
+	keywordOPTIONS    = "OPTIONS"
+	keywordVersion    = "version"
+	keywordStyle      = "style"
+	keywordBasePath   = "basePath"
+	keywordAuth       = "auth"
+	keywordBody       = "body"
+	keywordQuery      = "query"
+	keywordErrors     = "errors"
+	keywordSummary    = "summary"
+	keywordTag        = "tag"
+	keywordDeprecated = "deprecated"
+	keywordRaw        = "raw"
+	keywordDefault    = "default"
+	keywordPrimary    = "primary"
+	keywordUnique     = "unique"
+	keywordUpdatedAt  = "updatedAt"
+	keywordRelation   = "relation"
+	keywordREST       = "rest"
+	keywordEvents     = "events"
+	keywordBearer     = "bearer"
+	keywordAPIKey     = "apiKey"
+	keywordBasic      = "basic"
+	keywordNone       = "none"
+	keywordTrue       = "true"
+	keywordFalse      = "false"
+	keywordNull       = "null"
+	keywordNow        = "now"
+	keywordUUID       = "uuid"
+	keywordString     = "string"
+	keywordInt        = "int"
+	keywordFloat      = "float"
+	keywordBoolean    = "boolean"
+	keywordTimestamp  = "timestamp"
+	keywordDate       = "date"
+	keywordAny        = "any"
 
 	newline = '\n'
 	eof     = -1
 )
+
+var keywords = map[string]itemType{
+	keywordNamespace:  itemKeywordNamespace,
+	keywordImport:     itemKeywordImport,
+	keywordType:       itemKeywordType,
+	keywordEnum:       itemKeywordEnum,
+	keywordModel:      itemKeywordModel,
+	keywordInput:      itemKeywordInput,
+	keywordAPI:        itemKeywordAPI,
+	keywordRPC:        itemKeywordRPC,
+	keywordStream:     itemKeywordStream,
+	keywordPublish:    itemKeywordPublish,
+	keywordSubscribe:  itemKeywordSubscribe,
+	keywordGET:        itemKeywordGET,
+	keywordPOST:       itemKeywordPOST,
+	keywordPUT:        itemKeywordPUT,
+	keywordPATCH:      itemKeywordPATCH,
+	keywordDELETE:     itemKeywordDELETE,
+	keywordHEAD:       itemKeywordHEAD,
+	keywordOPTIONS:    itemKeywordOPTIONS,
+	keywordVersion:    itemKeywordVersion,
+	keywordStyle:      itemKeywordStyle,
+	keywordBasePath:   itemKeywordBasePath,
+	keywordAuth:       itemKeywordAuth,
+	keywordBody:       itemKeywordBody,
+	keywordQuery:      itemKeywordQuery,
+	keywordErrors:     itemKeywordErrors,
+	keywordSummary:    itemKeywordSummary,
+	keywordTag:        itemKeywordTag,
+	keywordDeprecated: itemKeywordDeprecated,
+	keywordRaw:        itemKeywordRaw,
+	keywordDefault:    itemKeywordDefault,
+	keywordPrimary:    itemKeywordPrimary,
+	keywordUnique:     itemKeywordUnique,
+	keywordUpdatedAt:  itemKeywordUpdatedAt,
+	keywordRelation:   itemKeywordRelation,
+	keywordREST:       itemKeywordREST,
+	keywordEvents:     itemKeywordEvents,
+	keywordBearer:     itemKeywordBearer,
+	keywordAPIKey:     itemKeywordAPIKey,
+	keywordBasic:      itemKeywordBasic,
+	keywordNone:       itemKeywordNone,
+	keywordTrue:       itemKeywordTrue,
+	keywordFalse:      itemKeywordFalse,
+	keywordNull:       itemKeywordNull,
+	keywordNow:        itemKeywordNow,
+	keywordUUID:       itemKeywordUUID,
+	keywordString:     itemKeywordString,
+	keywordInt:        itemKeywordInt,
+	keywordFloat:      itemKeywordFloat,
+	keywordBoolean:    itemKeywordBoolean,
+	keywordTimestamp:  itemKeywordTimestamp,
+	keywordDate:       itemKeywordDate,
+	keywordAny:        itemKeywordAny,
+}
 
 type item struct {
 	typ  itemType
@@ -45,11 +246,26 @@ func (i item) String() string {
 		return i.val
 	case itemEOF:
 		return "EOF"
-	default:
+	case itemNewline:
+		return "\\n"
+	case itemComment:
+		return "comment"
+	case itemIdent:
+		return fmt.Sprintf("ident(%s)", i.val)
+	case itemString:
+		return fmt.Sprintf("string(%s)", i.val)
+	case itemInt:
+		return fmt.Sprintf("int(%s)", i.val)
+	case itemFloat:
+		return fmt.Sprintf("float(%s)", i.val)
+	case itemUnrecognized:
 		if len(i.val) > 10 {
-			return fmt.Sprintf("%.10q...", i.val[:10])
+			return fmt.Sprintf("unrecognized(%.10q...)", i.val[:10])
 		}
-		return fmt.Sprintf("%q", i.val)
+		return fmt.Sprintf("unrecognized(%q)", i.val)
+	default:
+		// For all keyword and symbol tokens, just return the value
+		return i.val
 	}
 }
 
